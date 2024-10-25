220da14ad447c4efc6b88cc72e554a934b7f2d3a
Jesus-sanriv <jesusarmandosanriv@gmail.com>
2024-10-25 17:40:30 -0600

    Add files via upload

---
+++ myScript2.js
@@ -0,0 +1,11 @@
+function myFunction() {
+document.getElementById('demo3').style.fontSize='35px';
+document.getElementById("demo3").innerHTML = "Desde $1,458,900 M.N.";
+document.getElementById("demo3").style.color = "Green";
+document.getElementById("demo3").style.fontFamily = "Sanz Seriff";
+}
+
+function aumentar(){
+document.getElementById('demo2').style.fontSize='35px';
+document.getElementById('demo2').style.color = 'red';
+}
